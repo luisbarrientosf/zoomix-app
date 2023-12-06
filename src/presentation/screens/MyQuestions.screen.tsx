895ca1597984b623d4e0ac23e8553eee62f76365
Luis Barrientos <luis.barrientos.faj@gmail.com>
2023-12-06 18:11:02 -0300

    feat: back button to home screen

--- src/presentation/screens/MyQuestions.screen.tsx
+++ src/presentation/screens/MyQuestions.screen.tsx
@@ -1,15 +1,17 @@
 import { useEffect } from "react";
-import { StyleSheet, Text } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { FontAwesome } from "@expo/vector-icons";
 import { getMyQuestions } from "../../infrastructure/redux/actions/getMyQuestions.actions";
 import { useAppDispatch, useAppSelector } from "../../infrastructure/redux/hooks";
 import { QuestionList } from "../components/QuestionList/QuestionList";
 
+
 type MyQuestionsScreenParams = {
-  route: any;
+  navigation: any;
 }
 
-export const MyQuestionsScreen = ({ route } : MyQuestionsScreenParams) => {
+export const MyQuestionsScreen = ({ navigation } : MyQuestionsScreenParams) => {
   const loading = useAppSelector(state => state.getMyQuestions.loading);
   const questions = useAppSelector(state => state.getMyQuestions.value);
   const dispatch = useAppDispatch();
@@ -22,10 +24,20 @@ export const MyQuestionsScreen = ({ route } : MyQuestionsScreenParams) => {
 
   return (
     <SafeAreaView style={styles.container}>
+      
       { loading && <Text>Loading...</Text> }
       { !loading && (questions !== null && questions.length > 0) && (
         <>
-          <Text style={styles.title}>My Questions</Text>
+          <View style={styles.header}>
+            <Text style={styles.title}>My Questions</Text>
+            <FontAwesome
+              onPress={() => navigation.pop()}
+              name={"arrow-left"}
+              size={28}
+              color={"#AAAAAA"}
+            />
+          </View>
+          
           <QuestionList data={questions}/>
         </>
       )}
@@ -39,10 +51,16 @@ const styles = StyleSheet.create({
     width: "100%",
     paddingHorizontal: 12
   },
+  header: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginTop: 10,
+    marginBottom: 30,
+    paddingHorizontal: 5,
+  },
   title: {
     fontWeight: "600",
     fontSize: 30,
-    marginTop: 10,
-    marginBottom: 30
   }
 });
--- src/presentation/screens/QuestionList.screen.tsx
+++ src/presentation/screens/QuestionList.screen.tsx
@@ -1,14 +1,16 @@
-import { Button, StyleSheet, Text } from "react-native";
+import { Button, StyleSheet, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { FontAwesome } from "@expo/vector-icons";
 import { getQuestions } from "../../infrastructure/redux/actions/getQuestions.actions";
 import { useAppDispatch, useAppSelector } from "../../infrastructure/redux/hooks";
 import { QuestionCardCarousel } from "../../presentation/components/QuestionCardCarousel/QuestionCardCarousel";
 
 type QuestionListScreenParams = {
   route: any;
+  navigation: any;
 }
 
-export const QuestionListScreen = ({ route } : QuestionListScreenParams) => {
+export const QuestionListScreen = ({ route, navigation } : QuestionListScreenParams) => {
   const { category } = route.params;
   const loading = useAppSelector(state => state.getQuestions.loading);
   const questions = useAppSelector(state => state.getQuestions.value);
@@ -18,7 +20,17 @@ export const QuestionListScreen = ({ route } : QuestionListScreenParams) => {
     <SafeAreaView style={styles.container}>
       { loading && <Text>Loading...</Text> }
       { !loading && (questions !== null && questions.length > 0) && (
-        <QuestionCardCarousel data={questions}/>
+        <>
+          <View style={styles.header}>
+            <FontAwesome
+              onPress={() => navigation.pop()}
+              name={"arrow-left"}
+              size={28}
+              color={"#AAAAAA"}
+            />
+          </View>
+          <QuestionCardCarousel data={questions}/>
+        </>
       )}
 
       <Button
@@ -34,10 +46,16 @@ const styles = StyleSheet.create({
     flex: 1,
     width: "100%",
   },
+  header: {
+    flexDirection: "row",
+    justifyContent: "flex-end",
+    alignItems: "center",
+    marginTop: 10,
+    marginBottom: 30,
+    paddingHorizontal: 16
+  },
   title: {
     fontWeight: "600",
     fontSize: 30,
-    marginTop: 10,
-    marginBottom: 30
   }
 });
