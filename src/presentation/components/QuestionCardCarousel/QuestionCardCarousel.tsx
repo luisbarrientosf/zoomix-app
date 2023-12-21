7012033301b9ae3317b1e33c3613b13438c7703e
Luis Barrientos <luis.barrientos.faj@gmail.com>
2023-12-20 22:54:09 -0300

    feat: animated image background

--- src/presentation/components/QuestionCardCarousel/QuestionCardCarousel.tsx
+++ src/presentation/components/QuestionCardCarousel/QuestionCardCarousel.tsx
@@ -4,8 +4,10 @@ import {
   Animated,
   Dimensions,
   FlatList,
+  ImageURISource,
   ListRenderItem,
   StyleSheet,
+  View,
 } from "react-native";
 import { QuestionCard } from "../QuestionCard/QuestionCard";
 
@@ -22,13 +24,16 @@ export const QuestionCardCarousel: FC<QuestionCardCarouselProps> = ({ data }) =>
   const scrollX = useRef(new Animated.Value(0)).current;
   const flatListRef = useRef<FlatList<Question>>(null);
 
-  const AnimatedQuestionCard: ListRenderItem<Question> = ({ item, index }) => {
-    const inputRange = [
+  const getInputRange = (index: number) => {
+    return [
       (index - 1) * ITEM_LENGTH,
       (index ) * ITEM_LENGTH,
       (index + 1) * ITEM_LENGTH,
     ];
+  };
 
+  const AnimatedQuestionCard: ListRenderItem<Question> = ({ item, index }) => {
+    const inputRange = getInputRange(index);
     const translateY = scrollX.interpolate({
       inputRange,
       outputRange: [0, -TRANSLATE_Y, 0],
@@ -46,29 +51,76 @@ export const QuestionCardCarousel: FC<QuestionCardCarouselProps> = ({ data }) =>
     );
   };
 
+  interface AnimatedBackgroundProps {
+    item: ImageUri;
+    index: number;
+  }
+
+  interface ImageUri {
+    id: string;
+    uri: ImageURISource;
+  }
+
+  const AnimatedBackground: FC<AnimatedBackgroundProps> = ({ item, index }) => {
+    const inputRange = getInputRange(index);
+    const opacity = scrollX.interpolate({
+      inputRange,
+      outputRange: [0, 1, 0],
+      extrapolate: "clamp",
+    });
+    
+    return(
+      <Animated.Image
+        source={item.uri}
+        resizeMode={"contain"}
+        style={[
+          StyleSheet.absoluteFillObject,
+          { opacity }
+        ]}
+      />
+    );
+  };
+
+  const images: ImageUri[] = [
+    { id: "id1", uri: require("../../assets/backgrounds/wallhaven-8owy7o.png") },
+    { id: "id2", uri: require("../../assets/backgrounds/wallhaven-j8oo6m.png") },
+    { id: "id3", uri: require("../../assets/backgrounds/wallhaven-eyz75k.png") },
+  ];
 
   return (
-    <FlatList
-      ref={flatListRef}
-      data={data}
-      renderItem={AnimatedQuestionCard}
-      contentContainerStyle={styles.flatListContainer}
-      horizontal
-      showsHorizontalScrollIndicator={false}
-      bounces={false}
-      disableIntervalMomentum
-      decelerationRate="fast"
-      snapToInterval={ITEM_LENGTH}
-      snapToAlignment="start"
-      scrollEventThrottle={16}
-      renderToHardwareTextureAndroid
-      onScroll={
-        Animated.event(
-          [{ nativeEvent: { contentOffset: { x: scrollX } } }],
-          { useNativeDriver: false },
-        )
-      }
-    />
+    <>
+      <View style={styles.background}>
+        { images.map((item,index) => 
+          <AnimatedBackground 
+            key={item.id}
+            item={item} 
+            index={index}
+          />
+        )}
+      </View>
+      
+      <FlatList
+        ref={flatListRef}
+        data={data}
+        renderItem={AnimatedQuestionCard}
+        contentContainerStyle={styles.flatListContainer}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        bounces={false}
+        disableIntervalMomentum
+        decelerationRate="fast"
+        snapToInterval={ITEM_LENGTH}
+        snapToAlignment="start"
+        scrollEventThrottle={16}
+        renderToHardwareTextureAndroid
+        onScroll={
+          Animated.event(
+            [{ nativeEvent: { contentOffset: { x: scrollX } } }],
+            { useNativeDriver: false },
+          )
+        }
+      />
+    </>
   );
 };
 
@@ -77,5 +129,12 @@ const styles = StyleSheet.create({
     alignItems: "center",
     paddingLeft: PADDING,
     paddingRight: PADDING,
+  },
+  background: {
+    position: "absolute",
+    top: 0,
+    left:0,
+    width: "100%", 
+    height: "100%"
   }
 });
